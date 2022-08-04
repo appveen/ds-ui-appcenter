9f2b2198939de893338756ed535d0bebc751478f
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2022-08-04 11:19:06 +0530

    workflow overview services basic version

--- src/app/dashboard/workflow/workflow-overview/workflow-overview.component.ts
+++ src/app/dashboard/workflow/workflow-overview/workflow-overview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService, GetOptions } from 'src/app/service/common.service';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'odp-workflow-overview',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkflowOverviewComponent implements OnInit {
 
-  constructor() { }
+  showLazyLoader: boolean;
+  services: any;
+  constructor(
+    private commonService: CommonService
+  ) { }
+
 
   ngOnInit(): void {
+    const self = this;
+    self.services = [];
+    self.showLazyLoader = true;
+    self.getServices();
   }
 
+  getServices() {
+    const self = this;
+    const filter: any = { status: 'Active', app: self.commonService.app._id,'workflowConfig.enabled': true  };
+    if (!self.commonService.userDetails.isSuperAdmin
+      && self.commonService.servicesWithAccess.length > 0 && !self.commonService.isAppAdmin()) {
+      filter._id = {
+        $in: self.commonService.servicesWithAccess
+      };
+    }
+    const options: GetOptions = {
+      count: -1,
+      filter,
+      select: 'name',
+      sort: 'name'
+    };
+
+    self.commonService
+      .get('sm', `/${self.commonService.app._id}/service`, options)
+      .pipe(distinctUntilChanged())
+      .subscribe(res => {
+        {
+          self.services = res;
+          self.showLazyLoader = false;
+        }
+      });
+  }
 }
