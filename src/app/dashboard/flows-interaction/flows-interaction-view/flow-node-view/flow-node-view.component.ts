73e6a408f8f8cc1a6821d07b4ce260da918dfab2
Jugnu Agrawal <jugnu@appveen.com>
2022-10-12 14:35:07 +0530

    node execution time

--- src/app/dashboard/flows-interaction/flows-interaction-view/flow-node-view/flow-node-view.component.ts
+++ src/app/dashboard/flows-interaction/flows-interaction-view/flow-node-view/flow-node-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
 import { AppService } from 'src/app/service/app.service';
 import { CommonService } from 'src/app/service/common.service';
 import { FlowsInteractionService } from '../../flows-interaction.service';
@@ -69,5 +71,37 @@ export class FlowNodeViewComponent implements OnInit {
       this.fetchingData = false;
       this.commonService.errorToast(err);
     });
+  }
+
+  get duration() {
+    let text = '';
+    if (this.currNode && this.currNode._metadata && this.currNode._metadata.createdAt && this.currNode._metadata.lastUpdated) {
+      let startTime = new Date(this.currNode._metadata.createdAt).getTime();
+      let endTime = new Date(this.currNode._metadata.lastUpdated).getTime();
+      const duration = moment.duration(endTime - startTime);
+      text = duration.minutes() + ' min, ' + duration.seconds() + ' sec, ' + duration.milliseconds() + ' ms';
+      if (duration.hours() > 0) {
+        text = `${duration.hours()} hr, ` + text;
+      }
+      return text;
+    }
+    return '-';
+  }
+
+  get startTime() {
+    if (this.currNode && this.currNode._metadata && this.currNode._metadata.createdAt) {
+      return moment(this.currNode._metadata.createdAt).format("DD MM YYYY hh:mm:ss a");
+    }
+    return 'Not Yet started.';
+  }
+
+  get endTime() {
+    if (this.currNode && this.currNode._metadata && this.currNode._metadata.lastUpdated) {
+      return moment(this.currNode._metadata.lastUpdated).format("DD MM YYYY hh:mm:ss a");
+    }
+    if (!(this.currNode && this.currNode._metadata && this.currNode._metadata.createdAt)) {
+      return 'Not Yet started.';
+    }
+    return 'In Progress...';
   }
 }
